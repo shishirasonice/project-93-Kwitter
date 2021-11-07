b3001f76b3b029641ae42df15cd86442ec0fee1d
shishirasonice <82163635+shishirasonice@users.noreply.github.com>
2021-11-07 19:03:40 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyD3koNW8Y8P1R-L_0SlMM3d49bBdiZpQV8",
+    authDomain: "project-93-kwitter-ddf82.firebaseapp.com",
+    projectId: "project-93-kwitter-ddf82",
+    storageBucket: "project-93-kwitter-ddf82.appspot.com",
+    messagingSenderId: "623914366990",
+    appId: "1:623914366990:web:c75580070f5ad9f6fae45f"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
