73e4e42d5c6576b618b7a367771650b4fe283603
shishirasonice <82163635+shishirasonice@users.noreply.github.com>
2021-11-07 19:17:40 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -9,3 +9,18 @@ const firebaseConfig = {
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
+  user_name = localStorage.getItem("user_name");
+  document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!!"; 
+
+  function addRoom() {
+
+  function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey = childSnapshot.key;
+Room_names = childKey;
+
+       console.log("Room Name - " + Room_names);
+       row = "<div class= 'room_name' id= "+ Room_names + " onclick= 'redirectToRoomName(this.id)'> #" + Room_names + "</div> <hr>";
+       document.getElementById("output").innerHTML += row;
+
+});});}
+getData();
+  }
